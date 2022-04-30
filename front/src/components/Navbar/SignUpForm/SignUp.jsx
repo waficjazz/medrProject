ea45ae902eec12446335aac7635d48731e1c05c7
wafic <waficojazzaro@gmail.co-m>
2022-04-30 10:53:32 +0300

    all

--- front/src/components/Navbar/SignUpForm/SignUp.jsx
+++ front/src/components/Navbar/SignUpForm/SignUp.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import "./SignUp.css";
 const SignUp = () => {
-  return <div className="mainForm">dsgsdgsd</div>;
+  return (
+    <>
+      <div className="curtain">d</div>
+      <div className="mainForm">dsgsdgsd</div>
+    </>
+  );
 };
 
 export default SignUp;
