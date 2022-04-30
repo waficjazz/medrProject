ab7bd67c790902bc4a3a4a1760823757cf774e45
wafic <waficojazzaro@gmail.com>
2022-04-30 22:52:08 +0300

    main form

--- front/src/components/Navbar/SignUpForm/SignUp.jsx
+++ front/src/components/Navbar/SignUpForm/SignUp.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import "./SignUp.css";
+import { Step, Stepper, StepLabel } from "@mui/material";
 const SignUp = () => {
   return (
     <>
       <div className="curtain"></div>
-      <div className="mainForm">dsgsdgsd</div>
+      <div className="mainForm">
+        <Stepper>
+          <Step>
+            <StepLabel></StepLabel>
+          </Step>
+          <Step>
+            <StepLabel></StepLabel>
+          </Step>
+          <Step>
+            <StepLabel></StepLabel>
+          </Step>
+          <Step>
+            <StepLabel></StepLabel>
+          </Step>
+          <Step>
+            <StepLabel></StepLabel>
+          </Step>
+        </Stepper>
+      </div>
     </>
   );
 };
