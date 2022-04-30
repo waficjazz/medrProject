0566affa44217ed85ef7b8b3412a975a1e8d86f5
wafic <waficojazzaro@gmail.com>
2022-04-30 23:53:59 +0300

    ff

--- front/src/components/Navbar/SignUpForm/SignUp.jsx
+++ front/src/components/Navbar/SignUpForm/SignUp.jsx
@@ -1,29 +1,38 @@
 import React from "react";
 import "./SignUp.css";
-import { Step, Stepper, StepLabel, StepIcon, StepButton } from "@mui/material";
+import { Step, Stepper, StepLabel, StepIcon, StepButton, Typography } from "@mui/material";
+import { StyledEngineProvider } from "@mui/material/styles";
+import BadgeIcon from "@mui/icons-material/Badge";
+
+function testIcon() {
+  return <div className="QontoStepIcon-circle" />;
+}
+
 const SignUp = () => {
   return (
     <>
-      <div className="curtain"></div>
-      <div className="mainForm">
-        <Stepper>
-          <Step>
-            <StepButton></StepButton>
-          </Step>
-          <Step>
-            <StepLabel></StepLabel>
-          </Step>
-          <Step>
-            <StepLabel></StepLabel>
-          </Step>
-          <Step>
-            <StepLabel></StepLabel>
-          </Step>
-          <Step>
-            <StepLabel></StepLabel>
-          </Step>
-        </Stepper>
-      </div>
+      <StyledEngineProvider injectFirst>
+        <div className="curtain"></div>
+        <div className="mainForm">
+          <Stepper activeStep={3}>
+            <Step>
+              <StepLabel></StepLabel>
+            </Step>
+            <Step>
+              <StepLabel></StepLabel>
+            </Step>
+            <Step>
+              <StepLabel></StepLabel>
+            </Step>
+            <Step>
+              <StepLabel></StepLabel>
+            </Step>
+            <Step>
+              <StepLabel></StepLabel>
+            </Step>
+          </Stepper>
+        </div>
+      </StyledEngineProvider>
     </>
   );
 };
