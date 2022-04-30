313467e3947103c4f0ce9330d164db4f6f3b2b1a
wafic <waficojazzaro@gmail.co-m>
2022-04-30 17:11:42 +0300

    test routes

--- front/src/pages/MainPage/MainPage.jsx
+++ front/src/pages/MainPage/MainPage.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import SideBar from "../../components/Navbar/SideBar/SideBar";
 import SignUp from "../../components/Navbar/SignUpForm/SignUp";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./MainPage.css";
 const MainPage = () => {
   return (
     <>
       <div className="mainPage">
         <SideBar />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<div>hello</div>} />
+            <Route path="/h" element={<div>hello11</div>} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </>
   );
