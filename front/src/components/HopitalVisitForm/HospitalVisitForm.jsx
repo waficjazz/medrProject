3d608f4b347486cf42a093804f7f7ed075447e5f
wafic <waficojazzaro@gmail.com>
2022-05-12 23:51:30 +0300

    hosital update form

--- front/src/components/HopitalVisitForm/HospitalVisitForm.jsx
+++ front/src/components/HopitalVisitForm/HospitalVisitForm.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./HopitalVisitForm.css";
 import { StyledEngineProvider } from "@mui/material/styles";
 import { TextField, Input } from "@mui/material";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
 const HospitalVisitForm = () => {
+  const [tabValue, setTabValue] = useState(0);
+  const handleChange = (event, newValue) => {
+    setTabValue(newValue);
+  };
   return (
-    <StyledEngineProvider>
+    <StyledEngineProvider injectFirst>
       <div className="hVisitForm">
         <div className="inside">
-          <Input size="small" datatype="dsd" />
+          <div>
+            <Tabs onChange={handleChange} value={tabValue} className="customTabs" TabIndicatorProps={{ sx: { background: "white", color: "red" } }}>
+              <Tab value="0" label="Choose A Hospital" />
+              <Tab value="1" label="Add A Hospital" />
+            </Tabs>
+          </div>
+          <div className="hopitalForm">
+            <TextField size="small" label="Hospital Name" variant="standard" className="hospitalInputs" />
+          </div>
         </div>
       </div>
     </StyledEngineProvider>
